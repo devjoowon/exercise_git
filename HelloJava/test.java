f81487a0392b9196ef4417034bef3d702e17b9de
Joowon <dev.joowon@gmail.com>
2023-02-03 14:24:31 +0900

    add WOW

--- HelloJava/test.java
+++ HelloJava/test.java
@@ -2,5 +2,9 @@ public class test {
   
   public static void main(String[] args) {
     System.out.println("Test!");
+    System.out.println("WOW!!!");
   }
 }
+
+// 1. 코드 수정 -> 스테이징 -> 커밋 -> 푸시 -> 깃허브 사이트에서 푸시 잘 되었는지 확인
+// 2. 깃허브 사이트에서 코드 수정 -> 커밋 -> 로컬 이클립스에서 pull
