7997142f30b525f85628237e42740659cd1ef829
JOOWON PARK <124244074+devjoowon@users.noreply.github.com>
2023-02-03 14:25:38 +0900

    Update test.java

--- HelloJava/test.java
+++ HelloJava/test.java
@@ -3,6 +3,7 @@ public class test {
   public static void main(String[] args) {
     System.out.println("Test!");
     System.out.println("WOW!!!");
+    System.out.println("SUCCESS!!!!!");
   }
 }
 
